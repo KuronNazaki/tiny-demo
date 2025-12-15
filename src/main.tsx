bdde8862fc73d7100796ab402d4f71e3bd9d7b9f
Huynh Huy <huynhhuy.forwork@gmail.com>
2025-12-15 21:37:55 +0700

    feat: modify PWA

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from "virtual:pwa-register";
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('Offline ready')
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
