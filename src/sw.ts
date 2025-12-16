2a0015b58610d5e7fdc81cc49d65288d74db4089
Huynh Hoang Huy <huynhhuy.forwork@gmail.com>
2025-12-16 17:47:24 +0700

    feat: change

--- src/sw.ts
+++ src/sw.ts
@@ -4,7 +4,7 @@ import { clientsClaim } from "workbox-core";
 
 declare const self: ServiceWorkerGlobalScope;
 
-const _VERSION_TEST = "v0.0.2";
+const _VERSION_TEST = "v0.0.3";
 
 precacheAndRoute(self.__WB_MANIFEST);
 
