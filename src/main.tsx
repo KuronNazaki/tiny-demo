2a38bb08e258ddda1b150e0df97d7843769e32a2
Huynh Hoang Huy <huynhhuy.forwork@gmail.com>
2025-12-16 13:21:20 +0700

    feat: add layouts

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { registerSW } from "virtual:pwa-register";
-// import PullToRefresh from "pulltorefreshjs";
+import PullToRefresh from "pulltorefreshjs";
 
 // Check if the app is in standalone mode on iOS
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-// const isInWebAppiOS = (window.navigator as unknown as any).standalone === true;
+const isInWebAppiOS = (window.navigator as unknown as any).standalone === true;
 
-// if (isInWebAppiOS) {
-//   PullToRefresh.init({
-//     mainElement: "body", // Or the specific scrollable element
-//     onRefresh() {
-//       // The action to perform when refreshed (e.g., reload page, fetch new data)
-//       window.location.reload();
-//     },
-//   });
-// }
+if (isInWebAppiOS) {
+  PullToRefresh.init({
+    mainElement: "body", // Or the specific scrollable element
+    onRefresh() {
+      // The action to perform when refreshed (e.g., reload page, fetch new data)
+      window.location.reload();
+    },
+  });
+}
 
 // add this to prompt for a refresh
 const updateSW = registerSW({
