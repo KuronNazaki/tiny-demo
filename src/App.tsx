cde977bc551346669d0d8c7b2c40d2185dc3b748
Huynh Hoang Huy <huynhhuy.forwork@gmail.com>
2025-12-16 12:54:18 +0700

    feat: add scrollable area

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   }, [count]);
 
   return (
-    <div className="flex flex-col justify-center items-center w-full h-dvh gap-5">
+    <div className="flex flex-col justify-center items-center w-full min-h-dvh gap-5 py-20">
       <div className="flex gap-5 justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -23,6 +23,41 @@ function App() {
         </a>
       </div>
       <h1 className="font-semibold">Tiny Demo 3</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
