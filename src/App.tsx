af090d1d2a21ccf0bf9fad85f599c581725ec2d1
Huynh Hoang Huy <huynhhuy.forwork@gmail.com>
2025-12-16 09:09:35 +0700

    feat: change resolution

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   }, [count]);
 
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen gap-5">
+    <div className="flex flex-col justify-center items-center w-full h-dvh gap-5">
       <div className="flex gap-5 justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -22,7 +22,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="font-semibold">Tiny Demo</h1>
+      <h1 className="font-semibold">Tiny Demo 1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
