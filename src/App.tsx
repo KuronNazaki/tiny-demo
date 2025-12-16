9e49fd7b975ca530ad8f3138299fc64dc9bf6076
Huynh Hoang Huy <huynhhuy.forwork@gmail.com>
2025-12-16 09:16:39 +0700

    feat: change scale

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="font-semibold">Tiny Demo 1</h1>
+      <h1 className="font-semibold">Tiny Demo 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
