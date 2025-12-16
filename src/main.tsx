70f4afe7eed20e0d57da85d443fe56eec90ba613
Huynh Hoang Huy <huynhhuy.forwork@gmail.com>
2025-12-16 17:21:39 +0700

    feat: handle new version

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,20 @@ import "./index.css";
 import App from "./App.tsx";
 import { registerSW } from "virtual:pwa-register";
 
-registerSW();
+const updateSW = registerSW({
+  onNeedRefresh() {
+    const userAgreed = window.confirm(
+      "Có phiên bản mới của ứng dụng! Tải lại ngay?"
+    );
+
+    if (userAgreed) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("Ứng dụng đã sẵn sàng chạy ngoại tuyến.");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
