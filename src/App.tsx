7c414dd713f07136b4969923976099f3457d1620
Huynh Huy <huynhhuy.forwork@gmail.com>
2025-12-17 23:23:39 +0700

    feat: update splash screens

--- src/App.tsx
+++ src/App.tsx
@@ -126,13 +126,13 @@ function App() {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
-        <h1 className="font-semibold">Tiny Demo 15</h1>
+        <h1 className="font-semibold">The Particles</h1>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
           </button>
         </div>
-        <p>
+        <p className="mt-10">
           What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
           and typesetting industry. Lorem Ipsum has been the industry's standard
           dummy text ever since the 1500s, when an unknown printer took a galley
